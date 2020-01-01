facd71b9d1f384d02eb140a49ffa68f5074432cb
Diego <dsanthony997@gmail.com>
2020-01-01 18:20:58 -0500

    [Add] Timer schedule

--- src/app/src/main/java/com/example/android_samples/MainActivity.java
+++ src/app/src/main/java/com/example/android_samples/MainActivity.java
@@ -3,9 +3,9 @@ package com.example.android_samples;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
-import androidx.fragment.app.FragmentActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- src/app/src/main/java/com/example/android_samples/SplashScreenActivity.java
+++ src/app/src/main/java/com/example/android_samples/SplashScreenActivity.java
@@ -6,9 +6,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.Timer;
 import java.util.TimerTask;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final int SPLASH_SCREEN_DELAY = 3000;
 
     private Context mContext = this;
 
@@ -23,9 +26,13 @@ public class SplashScreenActivity extends AppCompatActivity {
                 Intent  intent = new Intent()
                         .setClass(mContext, MainActivity.class);
                 startActivity(intent);
-                
+
                 finish();
             }
         };
+
+        Timer timer = new Timer();
+        timer.schedule(timerTask,SPLASH_SCREEN_DELAY);
+
     }
 }
