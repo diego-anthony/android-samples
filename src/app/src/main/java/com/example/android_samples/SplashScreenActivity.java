6aa85c939d041f27b4fbbd331b120c89bf083b0d
Diego <dsanthony997@gmail.com>
2020-01-01 18:03:42 -0500

    [Add] Behavior Splash

--- src/app/src/main/java/com/example/android_samples/SplashScreenActivity.java
+++ src/app/src/main/java/com/example/android_samples/SplashScreenActivity.java
@@ -2,13 +2,30 @@ package com.example.android_samples;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.TimerTask;
+
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private Context mContext = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                Intent  intent = new Intent()
+                        .setClass(mContext, MainActivity.class);
+                startActivity(intent);
+                
+                finish();
+            }
+        };
     }
 }
