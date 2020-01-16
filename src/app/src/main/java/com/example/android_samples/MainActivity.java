29fc79c9fe3c5fc93048c10a80b8753aacad83c9
Diego Salazar <dsanthony997@gmail.com>
2020-01-16 16:36:53 -0500

    [Fix]

--- src/app/src/main/java/com/example/android_samples/MainActivity.java
+++ src/app/src/main/java/com/example/android_samples/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android_samples;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
@@ -10,5 +11,46 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        t("onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        t("onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        t("onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        t("onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        t("onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        t("onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        t("onDestroy");
+    }
+
+    private void t(String message){
+        Toast.makeText(this,message, Toast.LENGTH_LONG).show();
     }
 }
