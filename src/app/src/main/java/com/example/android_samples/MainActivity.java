45fd3ddf310c4649598a1dc29844c17d58b157f5
Diego Salazar <dsanthony997@gmail.com>
2020-01-16 16:47:04 -0500

    [Add] Button Second Activity

--- src/app/src/main/java/com/example/android_samples/MainActivity.java
+++ src/app/src/main/java/com/example/android_samples/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.android_samples;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
 
 public class MainActivity extends FragmentActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_second_activity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSecondActivity();
+            }
+        });
         t("onCreate");
+    }
+    private void goToSecondActivity(){
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 
     @Override
--- src/app/src/main/java/com/example/android_samples/SecondActivity.java
+++ src/app/src/main/java/com/example/android_samples/SecondActivity.java
@@ -1,5 +1,6 @@
 package com.example.android_samples;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,14 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        setUpActionBar();
+    }
+
+    private void setUpActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(true);
+            actionBar.setTitle(getString(R.string.second_activity_title));
+        }
     }
 }
